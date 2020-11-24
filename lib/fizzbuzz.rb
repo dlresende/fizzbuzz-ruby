120dd4f57a1a8c888414246c6d6a66cef807a64b
Diego Lemos <diego@diegolemos.net>
2020-11-24 22:34:40 +0000

    Remove ruby files

--- lib/fizzbuzz.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-require 'fizzbuzz/version'
-require 'fizzbuzz/runner'
--- spec/fizzbuzz_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.describe Fizzbuzz do
-  it 'has a version number' do
-    expect(Fizzbuzz::VERSION).not_to be nil
-  end
-end

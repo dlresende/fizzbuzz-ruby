fd4bef88e79e54d5eb139cdb21fe57aee9a227ae
Diego Lemos <diego@diegolemos.net>
2020-11-24 22:42:59 +0000

    ginkgo bootstrap -internal

---
+++ main_suite_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestFizzbuzz(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Fizzbuzz Suite")
+}
